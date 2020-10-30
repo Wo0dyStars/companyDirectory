39ee186f024ddbf37e47ad69e310004aa0bc05b7
Peter Kertesz <kerteszpetr@gmail.com>
2020-10-30 06:00:31 +0000

    Editing, saving and canceling save on profiles.

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { serverAPI } from "../services/serverAPI";
 
 class Employee extends React.Component {
     constructor(props) {
@@ -7,8 +8,16 @@ class Employee extends React.Component {
         this.state = {
             error: null,
             isLoaded: false,
-            employee: {}
+            employee: {},
+            editEmployee: {},
+            isEditing: false
         }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSave = this.handleSave.bind(this);
+        this.handleCancel = this.handleCancel.bind(this);
+        this.handleChangeName = this.handleChangeName.bind(this);
+        this.toggleEdit = this.toggleEdit.bind(this);
     }
 
     componentDidMount() {
@@ -20,7 +29,8 @@ class Employee extends React.Component {
                     console.log(result);
                     this.setState({
                         isLoaded: true,
-                        employee: result.data[0]
+                        employee: result.data[0],
+                        editEmployee: result.data[0]
                     })
                 },
                 (error) => {
@@ -32,8 +42,43 @@ class Employee extends React.Component {
             )
     }
 
+    handleChange = (event) => {
+        this.setState((prevState) => ({
+            editEmployee: { ...prevState.editEmployee, [event.target.name]: event.target.value }
+        }))
+    }
+
+    handleChangeName = (event) => {
+        const splitName = event.target.value.split(' ');
+        const firstName = splitName[0];
+        const lastName = splitName[1] ? splitName[1] : "";
+
+        this.setState((prevState) => ({
+            editEmployee: { ...prevState.editEmployee, firstName, lastName}
+        }))
+    }
+
+    handleCancel = (event) => {
+        this.setState({ editEmployee: this.state.employee });
+
+        this.toggleEdit();
+    }
+
+    handleSave = (event) => {
+        serverAPI("POST", "http://localhost/companydirectory/libs/php/updateEmployee.php", JSON.stringify(this.state.editEmployee))
+        .then((res) => console.log(res))
+        .catch((e) => console.log(e));
+
+        this.toggleEdit();
+    }
+
+    toggleEdit = (event) => {
+        this.setState({ isEditing: !this.state.isEditing });
+    }
+
     render() {
-        const { error, isLoaded, employee } = this.state;
+        const { error, isLoaded, employee, editEmployee, isEditing } = this.state;
+        console.log(this.state);
         if ( error ) {
             return <div>Error: {error.message}</div>
         } else if (!isLoaded) {
@@ -41,52 +86,97 @@ class Employee extends React.Component {
         } else {
             const isAvailable = employee.isAvailable ? "In Office" : "On Vacation";
 
+            let fields = {
+                email: <input type="text" defaultValue={ editEmployee.email } readOnly />,
+                phone: <input type="text" defaultValue={ editEmployee.phone } readOnly />,
+                name: <input type="text" defaultValue={ editEmployee.firstName + " " + editEmployee.lastName } readOnly />,
+                biography: <textarea defaultValue={ editEmployee.biography } readOnly></textarea>,
+                jobTitle: <input type="text" defaultValue={ editEmployee.jobTitle } readOnly />,
+                experience: <input type="text" defaultValue={ editEmployee.experience } readOnly />,
+                expertise: <input type="text" defaultValue={ editEmployee.expertise } readOnly />,
+                location: <input type="text" defaultValue={ editEmployee.location } readOnly />
+            }
+
+            if (isEditing) {
+                fields.email = <input type="text" name="email" placeholder={ editEmployee.email } onChange={this.handleChange} />;
+                fields.phone = <input type="text" name="phone" placeholder={ editEmployee.phone } onChange={this.handleChange} />;
+                fields.name = <input type="text" name="name" placeholder={ editEmployee.firstName + " " + editEmployee.lastName } onChange={this.handleChangeName} />;
+                fields.biography = <textarea name="biography" placeholder={ editEmployee.biography } onChange={this.handleChange}></textarea>;
+                fields.jobTitle = <input type="text" name="jobTitle" placeholder={ editEmployee.jobTitle } onChange={this.handleChange} />;
+                fields.experience = <input type="text" name="experience" placeholder={ editEmployee.experience } onChange={this.handleChange} />;
+                fields.expertise = <input type="text" name="expertise" placeholder={ editEmployee.expertise } onChange={this.handleChange} />;
+            }
+
             return (
                 <div>
                     <div className="employee__header">
-                        <div className="employee__header--avatar"> { employee.firstName[0] }{ employee.lastName[0] } </div>
+                        <div className="employee__header--avatar"> { editEmployee.firstName[0] }{ editEmployee.lastName[0] } </div>
                         <div className="employee__header--middle">
-                            <div className="employee__header--middle-fullname"> { employee.firstName } { employee.lastName } </div>
-                            <div className="employee__header--middle-department"> { employee.department } </div>
-                            <div className="employee__header--middle-employeeID"> { employee.id } </div>
+                            <div className="employee__header--middle-fullname"> { editEmployee.firstName } { editEmployee.lastName } </div>
+                            <div className="employee__header--middle-department"> { editEmployee.department } </div>
+                            <div className="employee__header--middle-employeeID"> Employee ID: { editEmployee.id } </div>
                             <div className="employee__header--middle-available"> { isAvailable } </div>
                         </div>
                         <div className="employee__header--right">
-                            <div className="employee__header--right-phone"> { employee.phone } </div>
-                            <div className="employee__header--right-email"> { employee.email } </div>
+                            <div className="employee__header--right-phone"> Mobile: { editEmployee.phone } </div>
+                            <div className="employee__header--right-email"> Email: { editEmployee.email } </div>
                         </div>
                     </div>
 
                     <hr/>
 
-                    <div>
-                        <div>
+                    <div className="employee__middle">
+                        <div className="employee__middle--left">
                             <div>
-                                <span>Job Title</span>
-                                <span>{ employee.jobTitle }</span>
+                                <label htmlFor="name">Name</label>
+                                { fields.name }
                             </div>
 
                             <div>
-                                <span>Experience</span>
-                                <span>{ employee.experience }</span>
+                                <label htmlFor="email">Email</label>
+                                { fields.email }
                             </div>
 
                             <div>
-                                <span>Expertise</span>
-                                <span>{ employee.expertise }</span>
+                                <label htmlFor="phone">Phone</label>
+                                { fields.phone }
                             </div>
 
                             <div>
-                                <span>Location</span>
-                                <span>{ employee.location }</span>
+                                <label htmlFor="jobTitle">Job Title</label>
+                                { fields.jobTitle }
                             </div>
 
-                        </div>
-                        
-                        <div>
-                            Biography
+                            <div>
+                                <label htmlFor="experience">Experience</label>
+                                { fields.experience }
+                            </div>
+
+                            <div>
+                                <label htmlFor="expertise">Expertise</label>
+                                { fields.expertise }
+                            </div>
+
+                            <div>
+                                <label htmlFor="location">Location</label>
+                                { fields.location }
+                            </div>
+                            
+                            <div>
+                                <label htmlFor="biography">Biography</label>
+                                { fields.biography }
+                            </div>
                         </div>
                     </div>
+
+                    { isEditing ? 
+                        ( 
+                            <div>
+                                <button type="button" onClick={this.handleCancel}>Cancel</button>
+                                <button type="button" onClick={this.handleSave}>Save</button>
+                            </div> 
+                        ) : <button type="button" onClick={this.toggleEdit}>Edit</button> }
+                    
                 </div>
             )
         }
